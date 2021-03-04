846270c8e44f4db1e224d074446e3eaeae8e4559
umitkadiroglu <umit-kadiroglu@hotmail.com>
2021-03-04 18:26:19 +0300

    week2

--- F1Token/src/contracts/F1Token.sol
+++ F1Token/src/contracts/F1Token.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "./EIP20Interface.sol";
+import "./EIP20Interface.sol"; // ERC-20 standartlarını import ettim.
 
 contract F1Token is EIP20Interface{
 
@@ -13,13 +13,15 @@ contract F1Token is EIP20Interface{
     string public symbol;
 
     constructor() public {
-        balances[msg.sender] = 1000000;
-        totalSupply = 1000000;
-        name = "F1Token";
-        decimals = 0;
-        symbol = "F1T";
+        balances[msg.sender] = 1000000; // kontrat oluşturulduğu gibi tüm tokenları oluşturucuya yolladım.
+        totalSupply = 1000000; // toplam arz
+        name = "F1Token"; // Ttken ismi
+        decimals = 0; // token decimalini küsuratlarla uğraşmamak için 0 yaptım.
+        symbol = "F1T"; // token sembolü
 
     }
+    // contract sahibinin adresinden belirli bir adrese token transferi
+    // eğer contract sahibinin bakiyesi gerekli koşulu sağlıyorsa, onun bakiyesinden değer düşülür ve hedef adresin bakiyesi artırılır.
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balances[msg.sender] >= _value);
         balances[msg.sender] -= _value;
@@ -28,6 +30,7 @@ contract F1Token is EIP20Interface{
         return true;
     }
 
+    // belirli bir adresten diğerine token transferi
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         uint256 allowance = allowed[_from][msg.sender];
         require(balances[_from] >= _value && allowance >= _value);
@@ -40,16 +43,18 @@ contract F1Token is EIP20Interface{
         return true;
     }
 
+    // contract sahibinin hesabında bulunan token sayısı
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
+    // contract sahibinin 3. parti bir dağıtıcı varsa, ona onay vermesi
     function approve(address _spender, uint256 _value) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
-
+    // dağıtıcının contract sahibinden izin aldığı miktar
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
